ec1c872ccb64ee7611aea046a40b7d16e3b512ec
Jorge H Baptista <jorgehenrique@live.com>
2022-06-25 22:51:42 -0300

    update id permissions seed

--- database/seeds/PermissionSeeder.js
+++ database/seeds/PermissionSeeder.js
@@ -5,11 +5,13 @@ class PermissionSeeder {
   async run() {
     await Permission.createMany([
       {
+        id: 1,
         name: "Administrador",
         slug: "master",
         description: "Permite gerenciar todas funcionalidades do sistema",
       },
       {
+        id: 2,
         name: "Corretor",
         slug: "corretor_novos_imoveis",
         description:
